1422a950b91a7d0a40b3238aaca3e3955fb6ac33
t-sequier <theo.sequier@iut-rodez.fr>
2021-11-17 08:32:50 +0100

    ajout du .bat serveur

---
+++ ClassLibrary2/DAO/CSequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2.DAO
+{
+    class CSequence
+    {
+        public static double getNextValue(String table, String connexionString)
+        {
+
+            OracleConnection connex = SingleConnexion.getInstance(connexionString);
+            OracleCommand req = new OracleCommand("select S_ID_" + table + ".nextval from dual", connex);
+            OracleDataReader res = req.ExecuteReader();
+
+            double value = -1;
+
+            if(res.HasRows)
+            {
+                res.Read();
+                value = double.Parse(res.GetValue(0).ToString());
+            }
+
+            connex.Close();
+            return value;
+        }
+
+    }
+}
---
+++ Run_IIS.bat
@@ -0,0 +1,2 @@
+
+"c:\Program Files x86\IIS Express\iisexpress.exe" /path:"Z:\WindowsFormsApp2\webservice"
